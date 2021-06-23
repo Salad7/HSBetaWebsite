48c1bb588264189334f83c0e92506742987ba7ef
Mohamed <kukgiplayer2@gmail.com>
2021-06-23 11:45:02 -0400

    update

---
+++ autoapply.py
@@ -0,0 +1,20 @@
+import pyautogui
+import selenium
+import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+#
+# browser = webdriver.Chrome()
+# wait = WebDriverWait(browser, 3)
+# visible = EC.visibility_of_element_located
+#
+# browser.get('https://www.youtube.com/watch?v=AFNUeUed8Ro&ab_channel=NDTV')
+# wait.until(visible((By.ID, "video-title")))
+# browser.find_element_by_id("video-title").click()
+# browser.minimize_window()
+# time.sleep(7)
+# browser.quit()
+import webbrowser
+webbrowser.open('https://nike.taleo.net/careersection/10100m/jobapply.ftl?job=203_NIKE_FT_EVG&lang=en&languageCode=en&countryCode=US&countryName=United%20States', new=2)
